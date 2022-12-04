9f71c9cc901357d610131a5f6a2f84282e2f267f
takahiro.tominaga <tt.300607@gmail.com>
2022-12-04 18:07:10 +0900

    style: リスト画面に写真等の情報を表示するよう変更 (#8)

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/main/component/OneRepository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/presentation/main/component/OneRepository.kt
@@ -1,16 +1,24 @@
 package jp.co.yumemi.android.code_check.presentation.main.component
 
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.fillMaxWidth
-import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.*
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.platform.testTag
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.layout.ContentScale
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.res.stringResource
+import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import coil.compose.AsyncImage
+import coil.request.ImageRequest
+import jp.co.yumemi.android.code_check.R
 import jp.co.yumemi.android.code_check.models.Repository
 
 @Composable
@@ -18,17 +26,52 @@ fun OneRepository(
     repository: Repository,
     onRepositoryClick: (Repository) -> Unit = {},
 ) {
-    Text(
+    Row(
         modifier = Modifier
             .fillMaxWidth()
             .clickable {
                 onRepositoryClick(repository)
             }
             .padding(horizontal = 16.dp, vertical = 8.dp),
-        text = repository.name,
-        fontSize = 16.sp,
-        color = MaterialTheme.colorScheme.onBackground,
-    )
+        verticalAlignment = Alignment.CenterVertically,
+    ) {
+        AsyncImage(
+            model = ImageRequest.Builder(LocalContext.current)
+                .data(repository.ownerIconUrl)
+                .crossfade(true)
+                .build(),
+            placeholder = painterResource(id = R.drawable.file_not_found),
+            contentDescription = "image of ${repository.name}",
+            contentScale = ContentScale.Crop,
+            alignment = Alignment.TopStart,
+            modifier = Modifier
+                .size(40.dp),
+        )
+
+        Spacer(modifier = Modifier.width(8.dp))
+
+        Column(
+            modifier = Modifier
+                .fillMaxWidth()
+        ) {
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = repository.name,
+                fontSize = 16.sp,
+                color = MaterialTheme.colorScheme.onBackground,
+            )
+
+            Text(
+                modifier = Modifier
+                    .fillMaxWidth(),
+                text = stringResource(id = R.string.written_language, repository.language),
+                fontSize = 12.sp,
+                color = Color.Gray,
+                textAlign = TextAlign.End,
+            )
+        }
+    }
     androidx.compose.material.Divider(
         modifier = Modifier
             .padding(0.dp),
@@ -36,13 +79,15 @@ fun OneRepository(
     )
 }
 
-@Preview
+@Preview(
+    showBackground = true,
+)
 @Composable
 fun OneRepositoryPreview() {
     val repository = Repository(
         name = "kokoichi206/kokoichi206",
         ownerIconUrl = "",
-        language = "",
+        language = "Kotlin",
         stargazersCount = 0,
         watchersCount = 1,
         forksCount = 2,
