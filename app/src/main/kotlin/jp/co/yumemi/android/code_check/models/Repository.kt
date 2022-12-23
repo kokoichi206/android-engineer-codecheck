cfa3c65cd8eb5d60e1bcfdab89cf6d6b7b5628d2
takahiro.tominaga <tt.300607@gmail.com>
2022-12-24 08:06:34 +0900

    refactor: Uriパッケージのエンコードを使うよう変更

--- app/src/main/kotlin/jp/co/yumemi/android/code_check/models/Repository.kt
+++ app/src/main/kotlin/jp/co/yumemi/android/code_check/models/Repository.kt
@@ -1,5 +1,6 @@
 package jp.co.yumemi.android.code_check.models
 
+import android.net.Uri
 import android.os.Parcelable
 import kotlinx.parcelize.Parcelize
 
@@ -13,4 +14,31 @@ data class Repository(
     val watchersCount: Long,
     val forksCount: Long,
     val openIssuesCount: Long,
-) : Parcelable
+) : Parcelable {
+
+    fun uriEncode(): Repository {
+        return Repository(
+            name = Uri.encode(name),
+            repoUrl = Uri.encode(repoUrl),
+            ownerIconUrl = Uri.encode(ownerIconUrl),
+            language = Uri.encode(language),
+            stargazersCount = stargazersCount,
+            watchersCount = watchersCount,
+            forksCount = forksCount,
+            openIssuesCount = openIssuesCount,
+        )
+    }
+
+    fun uriDecode(): Repository {
+        return Repository(
+            name = Uri.decode(name),
+            repoUrl = Uri.decode(repoUrl),
+            ownerIconUrl = Uri.decode(ownerIconUrl),
+            language = Uri.decode(language),
+            stargazersCount = stargazersCount,
+            watchersCount = watchersCount,
+            forksCount = forksCount,
+            openIssuesCount = openIssuesCount,
+        )
+    }
+}
